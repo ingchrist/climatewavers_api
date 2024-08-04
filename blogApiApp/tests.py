593fb8c1305491e55cc28f5b34c24c89065c73ac
root <root@LAPTOP-QO70EF0D.localdomain>
2024-08-04 15:50:11 -0700

    ok

--- blogApiApp/tests.py
+++ blogApiApp/tests.py
@@ -4,3 +4,9 @@ import requests
 request = requests.get('http://127.0.0.1:8000')
 
 print(request.json())
+                              
+                                               
+                                                                 
+                                                                                     
+                                                                                                      
+                                                                                                      
